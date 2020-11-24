529321fae854cfb7c25a2254e5a7ccd21e15aef5
kylelondon92 <kyle.london992@gmail.com>
2020-11-24 10:26:35 +0100

    Update missed reverse

--- nano/lib/numbers.cpp
+++ nano/lib/numbers.cpp
@@ -49,7 +49,7 @@ void nano::public_key::encode_account (std::string & destination_a) const
 		number_l >>= 5;
 		destination_a.push_back (account_encode (r));
 	}
-	destination_a.append ("_onan"); // nano_
+	destination_a.append ("_woem"); // meow_
 	std::reverse (destination_a.begin (), destination_a.end ());
 }
 
